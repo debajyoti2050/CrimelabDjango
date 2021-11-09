1953aeecdd14beae92786103d70041779865e8b9
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2021-11-09 13:22:07 +0530

    updated

--- app/views.py
+++ app/views.py
@@ -43,6 +43,67 @@ def crime_template(str):
     plt.savefig(buf, format="png", bbox_inches='tight')
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return data
+def state_ut_template(str):
+    temp1 = d[d['STATE/UT'].str.contains(str)]
+    #temp1 = temp1.sort_values(by='TOTAL',ascending=False)
+    tl=temp1['TOTAL'].tolist() 
+    temp1.plot(kind='bar',x='CRIME HEAD',y='TOTAL',color='red',figsize=(8,6))
+    plt.xticks(rotation='vertical')
+    plt.grid(b=True, color='purple',alpha=0.5)
+    plt.title(str, fontdict = font1)
+    plt.xlabel("LIST OF CRIMES", fontdict = font2)
+    plt.ylabel("RATE OF CRIMES", fontdict = font2)
+    for i in range(12):
+        plt.text(x=i-0.3, y = tl[i]+2, s = tl[i], size = 10)
+    buf = BytesIO()
+    plt.savefig(buf, format="png", bbox_inches='tight')
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+def crime_by_year_template(str):
+    y=d[d['STATE/UT'].str.contains('ALL-INDIA')]
+    sc=y.loc[:,['CRIME HEAD',str]]
+    #sc = sc.sort_values(by=str,ascending=False)
+    tl=sc[str].tolist()
+    sc.plot(kind='bar',x='CRIME HEAD',y=str,color='red',figsize=(8,4))
+    plt.xticks(rotation="vertical")
+    plt.grid(b=True, color='grey',alpha=0.5)
+    plt.title(str, fontdict = font1)
+    plt.xlabel("TYPE OF CRIMES", fontdict = font2)
+    plt.ylabel("RATE OF CRIME", fontdict = font2)
+    for i in range(12):
+        plt.text(x=i-0.3, y = tl[i]+10, s = tl[i], size = 10)
+    buf = BytesIO()
+    plt.savefig(buf, format="png", bbox_inches='tight')
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+def all_states_and_ut_templates(list,id,str,a,m,n): 
+    total_list=[]
+    for i in list:
+        temp1 = d[d['STATE/UT'].str.contains(i)] 
+        temp2=temp1.sum(axis=0)
+        temp2=temp2['TOTAL']
+        total_list.append(temp2)
+    data = {'STATE/UT':list, 'TOTAL':total_list}
+    ts=pd.DataFrame(data)
+    ts = ts.sort_values(by ='TOTAL' ,ascending=False)
+    tl=ts['TOTAL'].tolist() 
+    ts.plot(kind='bar',x='STATE/UT',y='TOTAL',color='red',figsize=(float(a),6))
+    plt.xticks(rotation='vertical')
+    plt.grid(b=True, color='purple',alpha=0.5)
+    plt.title(str, fontdict = font1)
+    for i in range(int(id)):
+        plt.text(x=i-float(m), y = tl[i]+int(n), s = tl[i], size = 10)
+    plt.xlabel("LIST OF CRIMES", fontdict = font2)
+    plt.ylabel("RATE OF CRIMES", fontdict = font2)
+    
+    buf = BytesIO()
+    plt.savefig(buf, format="png", bbox_inches='tight')
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+    
 
 
 # Create your views here.
